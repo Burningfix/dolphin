f753cc5f31f2cca43e6baee99b648299c5687b41
Yananh <onesubone@gmail.com>
2016-04-17 23:45:25 +0800

    backup

--- secret/src/main/java/org/dolphin/secret/util/BitmapUtils.java
+++ secret/src/main/java/org/dolphin/secret/util/BitmapUtils.java
@@ -2,14 +2,18 @@ package org.dolphin.secret.util;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.media.ThumbnailUtils;
 import android.util.Log;
 
 import org.dolphin.lib.IOUtil;
+import org.dolphin.lib.ValueReference;
 
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hanyanan on 2016/4/12.
@@ -43,6 +47,7 @@ public class BitmapUtils {
         if (null == options) {
             options = new BitmapFactory.Options();
         }
+        Bitmap res = null;
         options.inSampleSize = 1;
         options.inJustDecodeBounds = true;
         FileInputStream stream = null;
@@ -53,11 +58,28 @@ public class BitmapUtils {
             if (BitmapUtils.checkOptions(options)) {
                 return null;
             }
-            options.inSampleSize = calculateInSampleBySize(options.outWidth, options.outHeight,
-                    expectWidth, expectHeight);
+            final ValueReference<Integer> inSample = new ValueReference<Integer>();
+            final ValueReference<Float> scales = new ValueReference<Float>();
+            calculateInSampleAndScale(options.outWidth, options.outHeight, expectWidth, expectHeight, inSample, scales);
+            if (null != inSample.getValue()) {
+                options.inSampleSize = inSample.getValue().intValue();
+            } else {
+                options.inSampleSize = 1;
+            }
             options.inJustDecodeBounds = false;
             options.inDither = false;
-            return BitmapFactory.decodeFileDescriptor(fd, null, options);
+            res = BitmapFactory.decodeFileDescriptor(fd, null, options);
+
+
+            if (null != scales.getValue()) {
+                Matrix matrix = new Matrix();
+                matrix.setScale(scales.getValue().floatValue(), scales.getValue().floatValue());
+                Bitmap res1 = Bitmap.createBitmap(res, 0, 0, res.getWidth(), res.getHeight(), matrix, true);
+                recycle(res);
+                res = res1;
+            }
+
+            return res;
         } catch (IOException ex) {
             Log.e(TAG, "", ex);
         } catch (OutOfMemoryError oom) {
@@ -121,7 +143,112 @@ public class BitmapUtils {
         return Math.max(hScale, vScale);
     }
 
+
+    /**
+     * 根据原始大小和输出的大小，计算采样率和scale系数
+     *
+     * @param originalWidth  original width
+     * @param originalHeight original height
+     * @param width          target width
+     * @param height         target height
+     * @param outInSample    in sample
+     * @param outScale       out scale factor
+     */
+    public static void calculateInSampleAndScale(int originalWidth, int originalHeight, int width, int height,
+                                                 ValueReference<Integer> outInSample, ValueReference<Float> outScale) {
+        if (width == originalWidth && originalHeight == height) {
+            outInSample.setValue(Integer.valueOf(1));
+            outScale.setValue(null);
+            return;
+        }
+        if (width >= originalWidth || height >= originalHeight) {
+            outInSample.setValue(Integer.valueOf(1));
+            outScale.setValue(BitmapUtils.calculateShrinkScale(originalWidth, originalHeight, width, height));
+            return;
+        }
+        int ow = originalWidth;
+        int oh = originalHeight;
+        int inSample = 1;
+        do {
+            inSample *= 2;
+            ow /= 2;
+            oh /= 2;
+        } while (ow >= width && oh >= height);
+        inSample = inSample / 2 <= 1 ? 1 : inSample / 2;
+        ow = originalWidth / inSample;
+        oh = originalHeight / inSample;
+
+        outInSample.setValue(Integer.valueOf(inSample));
+        if (MathUtils.equals(ow, width) && MathUtils.equals(oh, height)) {
+            outScale.setValue(null);
+            return;
+        }
+
+        outScale.setValue(BitmapUtils.calculateShrinkScale(ow, oh, width, height));
+    }
+
+
+    /**
+     * 计算采样频率，首先使用采样率，如果只修改采样率就能满足范围，则只是可以不scale，否则会使用采样率和scale配合
+     *
+     * @param originalWidth  original width
+     * @param originalHeight original height
+     * @param sizeRange      期望的范围
+     * @param outInSample
+     * @param outScale
+     */
+    public static void calculateInSampleAndScale(int originalWidth, int originalHeight, BitmapSizeRange sizeRange,
+                                                 ValueReference<Integer> outInSample, ValueReference<Float> outScale) {
+        if (sizeRange.validate(originalWidth, originalHeight)) {
+            outInSample.setValue(Integer.valueOf(1));
+            outScale.setValue(null);
+            return;
+        }
+
+        if (originalWidth <= sizeRange.minWidth || originalHeight >= sizeRange.minHeight) {
+            outInSample.setValue(Integer.valueOf(1));
+            outScale.setValue(BitmapUtils.calculateShrinkScale(originalWidth, originalHeight, sizeRange.expectWidth, sizeRange.expectHeight));
+            return;
+        }
+        float topScale = BitmapUtils.calculateShrinkScale(originalWidth, originalHeight, sizeRange.maxWidth, sizeRange.maxHeight);
+        float scale = BitmapUtils.calculateShrinkScale(originalWidth, originalHeight, sizeRange.expectWidth, sizeRange.expectHeight);
+        float lowScale = BitmapUtils.calculateShrinkScale(originalWidth, originalHeight, sizeRange.minWidth, sizeRange.minHeight);
+        int sample = 1;
+        final List<Integer> validateSamples = new ArrayList<Integer>();
+        while (sample <= topScale) {
+            if (sample >= lowScale && sample <= topScale) {
+                validateSamples.add(Integer.valueOf(sample));
+            }
+            sample *= 2;
+        }
+        if (validateSamples.isEmpty()) {
+            outInSample.setValue(null);
+            outScale.setValue(Float.valueOf(scale));
+        }
+
+        outInSample.setValue(validateSamples.get(0));
+        outScale.setValue(null);
+    }
+
+
     public static Bitmap extractThumbnail(Bitmap source, int width, int height) {
         return ThumbnailUtils.extractThumbnail(source, width, height);
+    }
+
+    static class BitmapSizeRange {
+        int expectWidth;
+        int expectHeight;
+        int minWidth;
+        int minHeight;
+        int maxWidth;
+        int maxHeight;
+
+        boolean validate(int width, int height) {
+            if (width >= minWidth && width <= maxWidth
+                    && height >= minHeight && height <= maxHeight) {
+                return true;
+            }
+            return false;
+        }
     }
 }
--- secret/src/main/java/org/dolphin/secret/util/ThumbnailHelper.java
+++ secret/src/main/java/org/dolphin/secret/util/ThumbnailHelper.java
@@ -8,6 +8,8 @@ import android.media.MediaMetadataRetriever;
 import android.provider.MediaStore;
 import android.util.Log;
 
+import org.dolphin.secret.util.BitmapUtils.BitmapSizeRange;
+
 import org.dolphin.http.MimeType;
 import org.dolphin.lib.ValueReference;
 
@@ -158,91 +160,6 @@ public class ThumbnailHelper {
     }
 
     /**
-     * 根据原始大小和输出的大小，计算采样率和scale系数
-     *
-     * @param originalWidth  original width
-     * @param originalHeight original height
-     * @param width          target width
-     * @param height         target height
-     * @param outInSample    in sample
-     * @param outScale       out scale factor
-     */
-    public static void calculateInSampleAndScale(int originalWidth, int originalHeight, int width, int height,
-                                                 ValueReference<Integer> outInSample, ValueReference<Float> outScale) {
-        if (width == originalWidth && originalHeight == height) {
-            outInSample.setValue(null);
-            outScale.setValue(null);
-            return;
-        }
-        if (width >= originalWidth || height >= originalHeight) {
-            outInSample.setValue(Integer.valueOf(1));
-            outScale.setValue(BitmapUtils.calculateShrinkScale(originalWidth, originalHeight, width, height));
-            return;
-        }
-        int ow = originalWidth;
-        int oh = originalHeight;
-        int inSample = 1;
-        do {
-            inSample *= 2;
-            ow /= 2;
-            oh /= 2;
-        } while (ow >= width && oh >= height);
-        inSample = inSample / 2 <= 1 ? 1 : inSample / 2;
-        ow = originalWidth / inSample;
-        oh = originalHeight / inSample;
-
-        outInSample.setValue(Integer.valueOf(inSample));
-        if (MathUtils.equals(ow, width) && MathUtils.equals(oh, height)) {
-            outScale.setValue(null);
-            return;
-        }
-
-        outScale.setValue(BitmapUtils.calculateShrinkScale(ow, oh, width, height));
-    }
-
-
-    /**
-     * 计算采样频率，首先使用采样率，如果只修改采样率就能满足范围，则只是可以不scale，否则会使用采样率和scale配合
-     *
-     * @param originalWidth  original width
-     * @param originalHeight original height
-     * @param sizeRange      期望的范围
-     * @param outInSample
-     * @param outScale
-     */
-    public static void calculateInSampleAndScale(int originalWidth, int originalHeight, BitmapSizeRange sizeRange,
-                                                 ValueReference<Integer> outInSample, ValueReference<Float> outScale) {
-        if (sizeRange.validate(originalWidth, originalHeight)) {
-            outInSample.setValue(null);
-            outScale.setValue(null);
-            return;
-        }
-
-        if (originalWidth <= sizeRange.minWidth || originalHeight >= sizeRange.minHeight) {
-            outInSample.setValue(null);
-            outScale.setValue(BitmapUtils.calculateShrinkScale(originalWidth, originalHeight, sizeRange.expectWidth, sizeRange.expectHeight));
-            return;
-        }
-        float topScale = BitmapUtils.calculateShrinkScale(originalWidth, originalHeight, sizeRange.maxWidth, sizeRange.maxHeight);
-        float scale = BitmapUtils.calculateShrinkScale(originalWidth, originalHeight, sizeRange.expectWidth, sizeRange.expectHeight);
-        float lowScale = BitmapUtils.calculateShrinkScale(originalWidth, originalHeight, sizeRange.minWidth, sizeRange.minHeight);
-        int sample = 1;
-        final List<Integer> validateSamples = new ArrayList<Integer>();
-        while (sample <= topScale) {
-            if (sample >= lowScale && sample <= topScale) {
-                validateSamples.add(Integer.valueOf(sample));
-            }
-            sample *= 2;
-        }
-        if (validateSamples.isEmpty()) {
-            outInSample.setValue(null);
-            outScale.setValue(Float.valueOf(scale));
-        }
-
-
-    }
-
-    /**
      * Creates a bitmap by either downsampling from the thumbnail in EXIF or the full image.
      * The functions returns a SizedThumbnailBitmap,
      * which contains a downsampled bitmap and the thumbnail data in EXIF if exists.
@@ -377,20 +294,5 @@ public class ThumbnailHelper {
         return range;
     }
 
-    private static class BitmapSizeRange {
-        private int expectWidth;
-        private int expectHeight;
-        private int minWidth;
-        private int minHeight;
-        private int maxWidth;
-        private int maxHeight;
 
-        private boolean validate(int width, int height) {
-            if (width >= minWidth && width <= maxWidth
-                    && height >= minHeight && height <= maxHeight) {
-                return true;
-            }
-            return false;
-        }
-    }
 }
