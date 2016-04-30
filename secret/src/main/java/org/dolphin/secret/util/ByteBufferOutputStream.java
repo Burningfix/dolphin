1f2514b58a9350b89717c6b14f3e866e34b37c09
Yananh <onesubone@gmail.com>
2016-04-30 23:18:03 +0800

    backup

--- secret/src/main/java/org/dolphin/secret/util/ByteBufferOutputStream.java
+++ secret/src/main/java/org/dolphin/secret/util/ByteBufferOutputStream.java
@@ -1,20 +1,50 @@
 package org.dolphin.secret.util;
 
+import org.dolphin.job.Log;
+
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by yananh on 2016/1/16.
  */
 public class ByteBufferOutputStream extends OutputStream {
+    private final String TAG = "ByteBufferOutputStream";
     public static final int BLOCK_SIZE = 1024;
-    public static final int MAX_BLOCK_COUNT = 1024;
+    public static final int MAX_BLOCK_COUNT = 512;
+    private static final List<byte[]> sBuffPool = new LinkedList<byte[]>();
+    private static final Set sBuffPoolRefs = new HashSet();
 
+    public synchronized static void maxSize(int maxSize) {
 
+    }
 
+    public synchronized static void maxBlockCount(int count) {
 
+    }
+
+    public synchronized static void trim() {
+
+    }
+
+    private synchronized static byte[] alloc() {
+        if (sBuffPool.size() <= 0) {
+            return null;
+        }
+        return sBuffPool.remove(0);
+    }
+
+    private synchronized static void free(byte[] block) {
+        if (!sBuffPoolRefs.contains(block)) {
+            Log.e(TAG, "Free a invalidate block!!!!!");
+            return;
+        }
+        sBuffPool.add(0, block);
+    }
 
 
     private final List<byte[]> data = new LinkedList<byte[]>();
@@ -40,7 +70,7 @@ public class ByteBufferOutputStream extends OutputStream {
         super.close();
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 }
