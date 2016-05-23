d061648570c948df3bddd9c02f61f4d093bc2b5e
Yananh <onesubone@gmail.com>
2016-05-23 23:36:03 +0800

    backup

--- secret/src/main/java/org/dolphin/secret/util/BitmapUtils.java
+++ secret/src/main/java/org/dolphin/secret/util/BitmapUtils.java
@@ -1,9 +1,11 @@
 package org.dolphin.secret.util;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
@@ -11,6 +13,7 @@ import android.util.Log;
 import org.dolphin.lib.ValueReference;
 import org.dolphin.lib.util.IOUtil;
 
+import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -240,6 +243,8 @@ public class BitmapUtils {
             this.tryExtraFromMediaStore = tryExtraFromMediaStore;
         }
 
+        protected abstract Context getContext();
+
         protected boolean isZoomImage() {
             return this.zoomImage;
         }
@@ -266,6 +271,16 @@ public class BitmapUtils {
             } else {
                 kind = MediaStore.Images.Thumbnails.MINI_KIND;
             }
+            Uri uri = Uri.fromFile(new File(path));
+
+            String[] projection = {MediaStore.Images.Thumbnails._ID, MediaStore.Images.Thumbnails.DATA};
+
+            MediaStore.
+
+                    MediaStore.Images.Thumbnails.getThumbnail(
+                    getContentResolver(), selectedImageUri,
+                    MediaStore.Images.Thumbnails.MINI_KIND,
+                    (BitmapFactory.Options) null);
 
             return null;
         }
@@ -342,6 +357,36 @@ public class BitmapUtils {
                 return false;
             }
         }
+    }
+
+    public static Bitmap getImageThumbnail(Context context, ContentResolver cr, String Imagepath) {
+        ContentResolver testcr = context.getContentResolver();
+        String[] projection = { MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID, };
+        String whereClause = MediaStore.Images.Media.DATA + " = '" + Imagepath + "'";
+        Cursor cursor = testcr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, whereClause,
+                null, null);
+        int _id = 0;
+        String imagePath = "";
+        if (cursor == null || cursor.getCount() == 0) {
+            return null;
+        }
+        if (cursor.moveToFirst()) {
+
+            int _idColumn = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+            int _dataColumn = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+
+            do {
+                _id = cursor.getInt(_idColumn);
+                imagePath = cursor.getString(_dataColumn);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inDither = false;
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        Bitmap bitmap = MediaStore.Images.Thumbnails.getThumbnail(cr, _id, Images.Thumbnails.MINI_KIND,
+                options);
+        return bitmap;
     }
 
     /**
