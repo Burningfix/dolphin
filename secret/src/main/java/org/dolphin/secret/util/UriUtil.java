eeffddcbaee5b56308c9855845824ce522be2695
Yananh <onesubone@gmail.com>
2016-07-24 23:36:26 +0800

    backup

--- secret/src/main/java/org/dolphin/secret/util/UriUtil.java
+++ secret/src/main/java/org/dolphin/secret/util/UriUtil.java
@@ -1,9 +1,12 @@
 package org.dolphin.secret.util;
 
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 
 /**
@@ -18,7 +21,9 @@ public class UriUtil {
      * @return the file path or null such as /sdcard/0/a.jpg
      */
     public static String getRealFilePath(final Context context, final Uri uri) {
-        if (null == uri) return null;
+        if (null == uri) {
+            return null;
+        }
         final String scheme = uri.getScheme();
         String data = null;
         if (scheme == null) {
@@ -38,29 +43,75 @@ public class UriUtil {
         return data;
     }
 
-    String type = Utils.ensureNotNull(intent.getType());
-    Log.d(TAG, "uri is " + uri);
-    if (uri.getScheme().equals("file") && (type.contains("image/"))) {
-        String path = uri.getEncodedPath();
-        Log.d(TAG, "path1 is " + path);
+    public static Uri getFileUri() {
+        if (uri.getScheme().
+
+                equals("file")
+
+                && (type.contains("image/")))
+
+        {
+            String path = uri.getEncodedPath();
+            Log.d(TAG, "path1 is " + path);
+            if (path != null) {
+                path = Uri.decode(path);
+                Log.d(TAG, "path2 is " + path);
+                ContentResolver cr = this.getContentResolver();
+                StringBuffer buff = new StringBuffer();
+                buff.append("(")
+                        .append(Images.ImageColumns.DATA)
+                        .append("=")
+                        .append("'" + path + "'")
+                        .append(")");
+                Cursor cur = cr.query(
+                        Images.Media.EXTERNAL_CONTENT_URI,
+                        new String[]{Images.ImageColumns._ID},
+                        buff.toString(), null, null);
+                int index = 0;
+                for (cur.moveToFirst(); !cur.isAfterLast(); cur
+                        .moveToNext()) {
+                    index = cur.getColumnIndex(Images.ImageColumns._ID);
+                    // set _id value
+                    index = cur.getInt(index);
+                }
+                if (index == 0) {
+                    //do nothing
+                } else {
+                    Uri uri_temp = Uri
+                            .parse("content://media/external/images/media/"
+                                    + index);
+                    Log.d(TAG, "uri_temp is " + uri_temp);
+                    if (uri_temp != null) {
+                        uri = uri_temp;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public static Uri FilePathToUri(Context context, String path) {
+
+        Log.d("TAG", "filePath is " + path);
         if (path != null) {
             path = Uri.decode(path);
-            Log.d(TAG, "path2 is " + path);
-            ContentResolver cr = this.getContentResolver();
+            Log.d("TAG", "path2 is " + path);
+            ContentResolver cr = context.getContentResolver();
             StringBuffer buff = new StringBuffer();
             buff.append("(")
-                    .append(Images.ImageColumns.DATA)
+                    .append(MediaStore.Images.ImageColumns.DATA)
                     .append("=")
                     .append("'" + path + "'")
                     .append(")");
             Cursor cur = cr.query(
-                    Images.Media.EXTERNAL_CONTENT_URI,
-                    new String[] { Images.ImageColumns._ID },
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    new String[]{MediaStore.Images.ImageColumns._ID},
                     buff.toString(), null, null);
             int index = 0;
             for (cur.moveToFirst(); !cur.isAfterLast(); cur
                     .moveToNext()) {
-                index = cur.getColumnIndex(Images.ImageColumns._ID);
+                index = cur.getColumnIndex(MediaStore.Images.ImageColumns._ID);
                 // set _id value
                 index = cur.getInt(index);
             }
@@ -70,11 +121,122 @@ public class UriUtil {
                 Uri uri_temp = Uri
                         .parse("content://media/external/images/media/"
                                 + index);
-                Log.d(TAG, "uri_temp is " + uri_temp);
+                Log.d("TAG", "uri_temp is " + uri_temp);
                 if (uri_temp != null) {
-                    uri = uri_temp;
+                    return uri_temp;
                 }
             }
+
         }
+        return null;
     }
+
+
+    /**
+     * 根据Uri获取图片绝对路径，解决Android4.4以上版本Uri转换
+     * 4.4 使用的content://com.android.providers.media.documents/document/image:3951
+     * content://media/external/images/media/3951
+     * http://stackoverflow.com/questions/20067508/get-real-path-from-uri-android-kitkat-new-storage-access-framework
+     * @param context
+     * @param uri
+     * @author yaoxing
+     * @date 2014-10-12
+     */
+    public static String getImageAbsolutePath(Context context, Uri uri) {
+        if (context == null || uri == null)
+            return null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                String[] split = docId.split(":");
+                String type = split[0];
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            } else if (isDownloadsDocument(uri)) {
+                String id = DocumentsContract.getDocumentId(uri);
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                return getDataColumn(context, contentUri, null, null);
+            } else if (isMediaDocument(uri)) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                String[] split = docId.split(":");
+                String type = split[0];
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                String selection = MediaStore.Images.Media._ID + "=?";
+                String[] selectionArgs = new String[] { split[1] };
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        } // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        String column = MediaStore.Images.Media.DATA;
+        String[] projection = { column };
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+
+
+
 }
